a34eecb4fb312de18c056dda4f8bf832c4f9331d
Zerro97 <khuibeom@gmail.com>
2021-10-06 17:07:18 +0900

    Changed mouse event to pointer event for compatibility between mobile and desktop, working on pinch gesture for zoom in

--- src/app/System/MouseListenerSystem.js
+++ src/app/System/MouseListenerSystem.js
@@ -8,16 +8,30 @@ import { hexToCanvas, isInsideHexagon, applyTransformation } from '../Util';
  */
 export class MouseListenerSystem extends System {
 	execute(delta, time) {
-		document.addEventListener('mousemove', e => {
-			e.preventDefault();
-			e.stopPropagation();
+		// Store pointer events (used for pinch gesture in mobile)
+		let evCache = new Array();
+		let prevDiff = {value: -1};
+
+		document.addEventListener('pointerdown', e => {
+			evCache.push(e);
 
 			this.checkTiles(e.clientX, e.clientY, 'hover');
 		});
 
-		document.addEventListener('click', e => {
-			e.preventDefault();
-			e.stopPropagation();
+		document.addEventListener('pointermove', e => {
+			this.checkPinchGesture(e, evCache, prevDiff);
+			this.checkTiles(e.clientX, e.clientY, 'hover');
+		});
+
+		document.addEventListener('pointerup', e => {
+			for (let i = 0; i < evCache.length; i++) {
+				if (evCache[i].pointerId == e.pointerId) {
+					evCache.splice(i, 1);
+					break;
+				}
+			}
+			// If the number of pointers down is less than two then reset diff tracker
+			if (evCache.length < 2) prevDiff.value = -1;
 
 			this.checkTiles(e.clientX, e.clientY, 'click');
 		});
@@ -40,6 +54,42 @@ export class MouseListenerSystem extends System {
 		// Stopping this system once listener is registered
 		// Init doesn't work since other components are not yet registered
 		this.stop();
+	}
+
+	checkPinchGesture(e, evCache, prevDiff) {
+		const screenStatus = this.queries.screenStatus.results[0].getMutableComponent(ScreenStatus);
+
+		// Find this event in the cache and update its record with this event
+		for (let i = 0; i < evCache.length; i++) {
+			if (e.pointerId == evCache[i].pointerId) {
+				evCache[i] = e;
+				break;
+			}
+		}
+
+		// If two pointers are down, check for pinch gestures
+		if (evCache.length == 2) {
+			// Calculate the distance between the two pointers
+			let dx = evCache[0].clientX - evCache[1].clientX;
+			let dy = evCache[0].clientY - evCache[1].clientY;
+			let curDiff = Math.hypot(dx, dy);
+
+			if (prevDiff.value > 0) {
+				// The distance between the two pointers has increased
+				if (curDiff > prevDiff.value && screenStatus.scaleX < 1.5 && screenStatus.scaleY < 1.5) {
+					screenStatus.scaleX += 0.1;
+					screenStatus.scaleY += 0.1;
+				}
+				// The distance between the two pointers has decreased
+				if (curDiff < prevDiff.value && screenStatus.scaleX > 0.5 && screenStatus.scaleY > 0.5) {
+					screenStatus.scaleX -= 0.1;
+					screenStatus.scaleY -= 0.1;
+				}
+			}
+
+			// Cache the distance for the next move event
+			prevDiff.value = curDiff;
+		}
 	}
 
 	checkTiles(mouseX, mouseY, type) {
